8045480facb062fcc87362ab6bd82551cd20dd87
김재민 <jameen@gimjaemins-MacBook-Pro.local>
2020-09-18 21:06:56 -0500

    Updates

---
+++ precache-manifest.4f7f4ce912c08d4331474f7bb6b409fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f267ae83bdb57a095676c1be87a54a1",
+    "url": "/NewYear-Timer/index.html"
+  },
+  {
+    "revision": "f381cd2981af07236ed8",
+    "url": "/NewYear-Timer/static/css/main.6b64107e.chunk.css"
+  },
+  {
+    "revision": "acd6d4f061729c5a2153",
+    "url": "/NewYear-Timer/static/js/2.5b53ece3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NewYear-Timer/static/js/2.5b53ece3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f381cd2981af07236ed8",
+    "url": "/NewYear-Timer/static/js/main.e68c3cae.chunk.js"
+  },
+  {
+    "revision": "07f5b5bba14d09f332fd",
+    "url": "/NewYear-Timer/static/js/runtime-main.5b40edb8.js"
+  }
+]);
