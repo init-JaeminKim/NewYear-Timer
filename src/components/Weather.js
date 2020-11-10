f3e1534d9c1c03775b4c9a32173471ae4de7288e
김재민 <jameen@gimjaemins-MacBook-Pro.local>
2020-11-10 13:05:47 -0600

    Weather Again

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,6 +3,7 @@ import '../css/Weather.css'
 import Spinner from 'react-spinkit'
 
 
+
 export default class Weather extends Component {
 
     state = {
@@ -24,8 +25,8 @@ export default class Weather extends Component {
                 {lat === null || long == null
                     ? <Spinner className="Loading" name="line-scale" color="rgb(252, 237, 210)" />
                     : <div className="row">
-                        <h3 className="block fade-in">{main}°F</h3>
-                        <img className="block fade-in" src={`http://openweathermap.org/img/wn/${icon}@2x.png`}></img>
+                        <h3 className="block fade-in">{parseInt(main)}°C</h3>
+                        <img className="block fade-in" src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt={""}></img>
                         <h3 className="block fade-in">{des}</h3>
                     </div>
                 }
@@ -33,7 +34,7 @@ export default class Weather extends Component {
 
         )
     }
-    
+
 
     _getCoordinate = () => {
         if (navigator.geolocation) {
@@ -43,19 +44,29 @@ export default class Weather extends Component {
                     lat: position.coords.latitude,
                     long: position.coords.longitude,
                 });
-                fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${this.state.lat}&lon=${this.state.long}&units=imperial&appid=${API_KEY}`)
-                .then(res => res.json())
-                .then((json) => {
-                    this.setState({
-                        main: json.main.temp,
-                        icon: json.weather[0].icon,
-                        des: json.weather[0].description
-                    })
+                fetch(`https://community-open-weather-map.p.rapidapi.com/weather?lat=${this.state.lat}&lon=${this.state.long}&lang=de&units=metric`, {
+                    "method": "GET",
+                    "headers": {
+                        "x-rapidapi-key": process.env.REACT_APP_API_KEY,
+                        "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
+                    }
                 })
+                    .then(response => response.json())
+                    .then(json => {
+                        this.setState({
+                            main: json.main.temp,
+                            icon: json.weather[0].icon,
+                            des: json.weather[0].description
+                        })
+                    })
+                    .catch(err => {
+                        console.error(err);
+                    });
             })
         }
-        else{
+        else {
             console.log("Fail..")
         }
-    } 
-} 
+    }
+}
+
